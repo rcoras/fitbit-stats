bd4d2eae0dec21999191e1ca3eebd655011199e8
rcoras <rcoras@yahoo.com>
2020-03-31 15:34:06 -0400

    Added state to guess to display

--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -1,55 +1,59 @@
 'use strict'
-var statesObj = {
-  "Alabama":"Montgomery",
-  "Alaska":"Juneau",
-  "Arizona":"Phoenix",
-  "Arkansas":"Little Rock",
-  "California":"Sacramento",
-  "Colorado":"Denver",
-  "Connecticut":"Hartford",
-  "Delaware":"Dover",
-  "Florida":"Tallahassee",
-  "Georgia":"Atlanta",
-  "Hawaii":"Honolulu",
-  "Idaho":"Boise",
-  "Illinois":"Springfield",
-  "Indiana":"Indianapolis",
-  "Iowa":"Des Moines",
-  "Kansas":"Topeka",
-  "Kentucky":"Frankfort",
-  "Louisiana":"Baton Rouge",
-  "Maine":"Augusta",
-  "Maryland":"Annapolis",
-  "Massachusetts":"Boston",
-  "Michigan":"Lansing",
-  "Minnesota":"St. Paul",
-  "Mississippi":"Jackson",
-  "Missouri":"Jefferson City",
-  "Montana":"Helena",
-  "Nebraska":"Lincoln",
-  "Nevada":"Carson City",
-  "New Hampshire":"Concord",
-  "New Jersey":"Trenton",
-  "New Mexico":"Santa Fe",
-  "New York":"Albany",
-  "North Carolina":"Raleigh",
-  "North Dakota":"Bismarck",
-  "Ohio":"Columbus",
-  "Oklahoma":"Oklahoma City",
-  "Oregon":"Salem",
-  "Pennsylvania":"Harrisburg",
-  "Rhode Island":"Providence",
-  "South Carolina":"Columbia",
-  "South Dakota":"Pierre",
-  "Tennessee":"Nashville",
-  "Texas":"Austin",
-  "Utah":"Salt Lake City",
-  "Vermont":"Montpelier",
-  "Virginia":"Richmond",
-  "Washington":"Olympia",
-  "West Virginia":"Charleston",
-  "Wisconsin":"Madison",
-  "Wyoming":"Cheyenne"
+const getFormFields = require('../../lib/get-form-fields')
+const store = require('./store')
+
+
+const statesObj = {
+  'Alabama': 'Montgomery',
+  'Alaska': 'Juneau',
+  'Arizona': 'Phoenix',
+  'Arkansas': 'Little Rock',
+  'California': 'Sacramento',
+  'Colorado': 'Denver',
+  'Connecticut': 'Hartford',
+  'Delaware': 'Dover',
+  'Florida': 'Tallahassee',
+  'Georgia': 'Atlanta',
+  'Hawaii': 'Honolulu',
+  'Idaho': 'Boise',
+  'Illinois': 'Springfield',
+  'Indiana': 'Indianapolis',
+  'Iowa': 'Des Moines',
+  'Kansas': 'Topeka',
+  'Kentucky': 'Frankfort',
+  'Louisiana': 'Baton Rouge',
+  'Maine': 'Augusta',
+  'Maryland': 'Annapolis',
+  'Massachusetts': 'Boston',
+  'Michigan': 'Lansing',
+  'Minnesota': 'St. Paul',
+  'Mississippi': 'Jackson',
+  'Missouri': 'Jefferson City',
+  'Montana': 'Helena',
+  'Nebraska': 'Lincoln',
+  'Nevada': 'Carson City',
+  'New Hampshire': 'Concord',
+  'New Jersey': 'Trenton',
+  'New Mexico': 'Santa Fe',
+  'New York': 'Albany',
+  'North Carolina': 'Raleigh',
+  'North Dakota': 'Bismarck',
+  'Ohio': 'Columbus',
+  'Oklahoma': 'Oklahoma City',
+  'Oregon': 'Salem',
+  'Pennsylvania': 'Harrisburg',
+  'Rhode Island': 'Providence',
+  'South Carolina': 'Columbia',
+  'South Dakota': 'Pierre',
+  'Tennessee': 'Nashville',
+  'Texas': 'Austin',
+  'Utah': 'Salt Lake City',
+  'Vermont': 'Montpelier',
+  'Virginia': 'Richmond',
+  'Washington': 'Olympia',
+  'West Virginia': 'Charleston',
+  'Wisconsin': 'Madison',
+  'Wyoming': 'Cheyenne'
 }
 
 function guessCapital(state){
@@ -65,12 +69,13 @@ function guessCapital(state){
 const onStartGame = function (event) {
   //get a random number and use that state in message and function for game
   const states = Object.keys(statesObj)
+  // $('.answer-box').css('display', '')
+  $('.answer-box').show()
   const randomIndex = Math.floor(Math.random() * (49 - 0)) + 0;
   let stateToGuess = states[randomIndex]
   console.log(stateToGuess)
-  $('#gameMessage').text('Guess the capital of', stateToGuess,'?')
+  $('#gameMessage').html('What\'s the capital of ').append(stateToGuess, "?")
   console.log('tried to update game message')
-  $('.form').show
 }
 
 
